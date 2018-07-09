6c90f84f2f5b63a074b3fefa9cc95e09632219a0
Jayms8462 <Jayms.Portune@gmail.com>
2018-07-08 19:34:06 -0700

    Fixed Layout Issues

--- js/sidenav.js
+++ js/sidenav.js
@@ -2,14 +2,15 @@
 
 var menu = true;
 
-function hideMenu(){
+function hideMenu() {
   document.write('<img src="/res/menu.png" id="menu" style="width:60px; opacity: 0.40" onclick="hide()">');
-  document.write('<div id="sideNav" style="display: none"><p>test</p></div>');
+  document.write('<div id="closeMenu" style="display: none"></div>');
+
 }
 
 function hide(){
   var target = document.getElementById("menu");
-  var targetMenu = document.getElementById("sideMenu");
+  var targetMenu = document.getElementById("sideNav");
   if (menu === false) {
     target.style.display = "inline-block";
   } else if (menu === true) {
